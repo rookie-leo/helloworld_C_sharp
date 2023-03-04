819120dc138ececa47b2028db484a992c2dbac69
rookie-leo <proleonardopa@gmail.com>
2023-03-04 17:28:50 -0300

    Estrutura de um programa em Visual Studio com C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblNome = new Label();
+            button1 = new Button();
+            txtMensagem = new TextBox();
+            SuspendLayout();
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Location = new Point(30, 22);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(40, 15);
+            lblNome.TabIndex = 0;
+            lblNome.Text = "Nome";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(30, 56);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // txtMensagem
+            // 
+            txtMensagem.Location = new Point(74, 19);
+            txtMensagem.Name = "txtMensagem";
+            txtMensagem.Size = new Size(100, 23);
+            txtMensagem.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(234, 298);
+            Controls.Add(txtMensagem);
+            Controls.Add(button1);
+            Controls.Add(lblNome);
+            Name = "Form1";
+            Text = "HelloWorld";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblNome;
+        private Button button1;
+        private TextBox txtMensagem;
+    }
+}
